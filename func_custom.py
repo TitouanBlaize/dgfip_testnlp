1a879094e9aa8b000bc9d696ce39bd93f5eb3713
TitouanBlaize <62878871+TitouanBlaize@users.noreply.github.com>
2024-05-21 20:37:19 +0200

    Update preprocess

--- func_custom.py
+++ func_custom.py
@@ -8,53 +8,30 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.stem import SnowballStemmer
 
+RE_SUSPICIOUS = re.compile(r'[&#<>{}\[\]\\]')
+
+def impurity(text, min_len = 10):
+    if text == None or len(text) < min_len:
+        return 0
+    else:
+        return len(RE_SUSPICIOUS.findall(text))/len(text)
+
 def replace_punctuation_with_space(text):
-    translator = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
+    translator = str.maketrans(string.punctuation, 
+                               ' ' * len(string.punctuation))
     return text.translate(translator)
 
-def preprocess_text_print(text):
+def preprocess_stopwords(text, stopwords):
     # Convert text to lowercase
     text = text.lower()
-    print(text)
+
+    # Uniformisation unicode
+    modified_text = unicodedata.normalize("NFKC", text)
 
     # Replace punctuation with spaces
     # Note : je préfère faire cela avant la tokenisation, lorsqu'un signe de ponctuation
     # est placé juste après un mot inconnu le tokenizer n'arrive pas à le séparer sinon
-    text_punct = replace_punctuation_with_space(text)
-
-    # Tokenization
-    tokens = word_tokenize(text_punct, language = "french")
-    print(tokens)
-
-    # Remove punctuation
-    # tokens = [token for token in tokens if token not in string.punctuation]
-    
-    # Remove stopwords
-    stop_words = (stopwords.words('french'))
-    anonymisation_stopword = ["xxxxx", "0000", "00000", "0000"]
-    stop_words.extend(anonymisation_stopword)
-    tokens = [token for token in tokens if token not in stop_words]
-    print(tokens)
-    # Remove single caracter (molecules often)
-    # tokens = [token for token in tokens if len(token) > 1]
-
-    # Lemmatisation ou racinisation
-    stemmer = SnowballStemmer('french')
-    tokens = [stemmer.stem(mot) for mot in tokens]
-    print(tokens)   
-    # Join tokens back into a single string
-    processed_text = ' '.join(tokens)
-    
-    return processed_text
-
-def preprocess_text(text):
-    # Convert text to lowercase
-    text = text.lower()
-
-    # Replace punctuation with spaces
-    # Note : je préfère faire cela avant la tokenisation, lorsqu'un signe de ponctuation
-    # est placé juste après un mot inconnu le tokenizer n'arrive pas à le séparer sinon
-    modified_text = replace_punctuation_with_space(text)
+    modified_text = replace_punctuation_with_space(modified_text)
 
     # Remove digits
     modified_text = re.sub(r'\d', '', modified_text)
@@ -62,51 +39,38 @@ def preprocess_text(text):
     # Tokenization
     tokens = word_tokenize(modified_text, language = "french")
     
-    # Remove stopwords
-    stop_words = (stopwords.words('french'))
-    anonymisation_stopword = ["xxxxxé", "xxxxxer", "xxxxx", "xx", "0000", "00000", "0000"]
-    stop_words.extend(anonymisation_stopword)
-    tokens = [token for token in tokens if token not in stop_words]
+    # Remove defined stopwords
+    tokens = [token for token in tokens if token not in stopwords]
+    
+    return tokens
+
+def preprocess_text(text, stopwords):
+    # Convert text to lowercase
+    text = text.lower()
+
+    # Uniformisation unicode
+    modified_text = unicodedata.normalize("NFKC", text)
+
+    # Replace punctuation with spaces
+    # Note : je préfère faire cela avant la tokenisation, lorsqu'un signe de ponctuation
+    # est placé juste après un mot inconnu le tokenizer n'arrive pas à le séparer sinon
+    modified_text = replace_punctuation_with_space(modified_text)
+
+    # Remove digits
+    modified_text = re.sub(r'\d', '', modified_text)
+
+    # Tokenization
+    tokens = word_tokenize(modified_text, language = "french")
+    
+    # Remove defined stopwords
+    tokens = [token for token in tokens if token not in stopwords]
 
     # Remove single caracter
     tokens = [token for token in tokens if len(token) > 1]
 
     # Lemmatisation ou racinisation
-    stemmer = SnowballStemmer('french')
-    tokens = [stemmer.stem(mot) for mot in tokens]
-    
-    # Join tokens back into a single string
-    processed_text = ' '.join(tokens)
-    
-    return processed_text
-
-def preprocess_text_save(text):
-    # Convert text to lowercase
-    text = text.lower()
-
-    punctuation_pattern = r'[^\w\s]'
-    # Replace punctuation with spaces using sub() function
-    modified_text = re.sub(punctuation_pattern, ' ', text)
-
-    # Remove digits
-    modified_text = re.sub(r'\d', '', modified_text)
-    
-    # Tokenization
-    tokens = word_tokenize(modified_text, language = "french")
-    
-    # Remove punctuation
-    tokens = [token for token in tokens if token not in string.punctuation]
-    
-    # Remove stopwords
-    stop_words = set(stopwords.words('french'))
-    tokens = [token for token in tokens if token not in stop_words]
-
-    # Remove single caracter (molecules often)
-    # tokens = [token for token in tokens if len(token) > 1]
-
-    # Lemmatisation ou racinisation
-    stemmer = SnowballStemmer('french')
-    tokens = [stemmer.stem(mot) for mot in tokens]
+    # stemmer = SnowballStemmer('french')
+    # tokens = [stemmer.stem(mot) for mot in tokens]
     
     # Join tokens back into a single string
     processed_text = ' '.join(tokens)
@@ -124,6 +88,8 @@ def count_words(sentence: str) -> int:
         int: The number of words in the sentence.
     """
     # Split the sentence into words using whitespace as delimiter
-    words = sentence.split()
+    # words = sentence.split()
+    
+    words = word_tokenize(sentence, language = "french")
     # Return the number of words
     return len(words)
